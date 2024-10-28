04d14391b298841975163e9b81d9603119189cda
moons soft <jeongsumoon26@gmail.com>
2024-10-28 15:44:42 +0900

    브렌치 테스트

---
+++ new.py
@@ -0,0 +1,26 @@
+print(5)
+print(-10)
+print(3.14)
+station = "사당"
+print(station+"행 열자가 들어 오고 있습니다.")
+print(not(1 != 3))
+
+print(pow(4, 2))
+
+from math import *
+print(floor(4.99))
+print(ceil(3.14))
+
+from random import *
+
+print(randrange(1, 46))
+
+print(randrange(1, 45))
+
+rand_Date = randint(4, 28)
+
+print("오프라인 스터디 모임 날짜는 매월" + str(rand_Date) + "일로 선정 되었습니다.")
+
+
+
+
